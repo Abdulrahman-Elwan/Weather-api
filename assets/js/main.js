e2a0597445f87b755d2f0cf5653a070a3345e703
Abdulrahman-Elwan <137184416+Abdulrahman-Elwan@users.noreply.github.com>
2025-07-06 13:04:21 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,7 @@ function renderCurrentWeather(){
         const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
         let d = new Date();
-
+        console.log(d.getDay());
         let cartona = `
             <div class="col-md-4 d-flex">
                 <div class="weather-card h-100 w-100 d-flex flex-column">
